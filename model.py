471e5e235a797d0a84e482eb3536c4c865122cb5
Tanya Gupta <2018eeb1149@iitrpr.ac.in>
2021-11-21 10:57:16 +0530

    3

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ class CSRNet(nn.Module):
             mod = models.vgg16(pretrained = True)
             self._initialize_weights()
             for i in xrange(len(self.frontend.state_dict().items())):
-                self.frontend.state_dict().items()[i][1].data[:] = mod.state_dict().items()[i][1].data[:]
+                self.frontend.state_dict().items()[i][1].data[:] = list(mod.state_dict().items())[i][1].data[:]
     def forward(self,x):
         x = self.frontend(x)
         x = self.backend(x)
